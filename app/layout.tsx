8d5cc9f53bea02d4858a89352d0052b2a0d72e9b
Rotios <jose.j.rivasgarcia@gmail.com>
2024-09-19 00:07:19 -0500

    Portfolio updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.ico",
+    icon: "/favicon.jpeg",
   },
 };
 
@@ -46,7 +46,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-4 flex-grow">
+            <main className="container mx-auto max-w-7xl pt-12 px-4 flex-grow">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,9 +11,46 @@ export default function ProjectsPage() {
         <h1 className={title()}>Projects</h1>
       </div>
 
-      <div className="flex gap-3">
+      <div className="flex gap-3 flex-col">
 
-        <Card className="max-w-[400px]">
+      <Card className="max-w-[500px]">
+          <CardHeader className="flex gap-3">
+            <Link color="foreground" href="https://game-stat-simulators.rotios.com/">
+            <Image
+              alt="Tribute Image"
+              height={50}
+              radius="md"
+              src="/GSSLogo.jpeg"
+              width={50}
+            />
+              <div className="flex flex-col text-left" style={{marginLeft: "5px"}}>
+                <p className="text-md">Game Stat Simulators</p>
+                <p className="text-small text-default-500">Lit JS website for caulcating stat increases.</p>
+              </div>
+            </Link>
+          </CardHeader>
+          <Divider/>
+          <CardBody className="text-left">
+            <p>
+              This website will help you calculate how many games you need to play in order to increase your stats by
+              any amount. It uses simple maths to calculate your expected average and also simulates matches until
+              your stats are reached. The simulations are entirely based on the stats you provide as input.
+            </p>
+            <br/>
+            <p>Game Stat Simulators was build using Lit JS and Vaadin components.</p>
+          </CardBody>
+          <Divider/>
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://game-stat-simulators.rotios.com/"
+            >
+              See it in action!
+            </Link>
+          </CardFooter>
+        </Card>
+        <Card className="max-w-[500px]">
           <CardHeader className="flex gap-3">
             <Link color="foreground" href="/projects/minesweeper">
             <Image
@@ -23,7 +60,7 @@ export default function ProjectsPage() {
               src="/minesweeper_detector.jpeg"
               width={50}
             />
-              <div className="flex flex-col text-left">
+              <div className="flex flex-col text-left" style={{marginLeft: "5px"}}>
                 <p className="text-md">Minesweeper</p>
                 <p className="text-small text-default-500">Game and Solver. Try it out!</p>
               </div>
@@ -49,8 +86,46 @@ export default function ProjectsPage() {
             </Link>
           </CardFooter>
         </Card>
-      </div>
 
+        <Card className="max-w-[500px]">
+          <CardHeader className="flex gap-3">
+            <Link color="foreground" href="https://rotios.github.io/The-Adventures-of-Fat-Tone/">
+            <Image
+              alt="Tribute Image"
+              height={50}
+              radius="md"
+              src="/TAOFT.jpeg"
+              width={50}
+            />
+              <div className="flex flex-col text-left" style={{marginLeft: "5px"}}>
+                <p className="text-md">The Adventures of Fat Tone</p>
+                <p className="text-small text-default-500">Roguelike game using Rot.JS!</p>
+              </div>
+            </Link>
+          </CardHeader>
+          <Divider/>
+          <CardBody className="text-left">
+            <p>A short Roguelike game written with the Rot.JS library. In conjunction with <Link
+                isExternal
+                href="https://www.github.com/jcf1"
+              >
+                John Freeman.
+              </Link>
+            </p>
+
+          </CardBody>
+          <Divider/>
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://rotios.github.io/The-Adventures-of-Fat-Tone/"
+            >
+              See it in action!
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
