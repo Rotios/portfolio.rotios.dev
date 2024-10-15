5840b19fd00b2941758ae24719873296dc0110e4
Rotios <jose.j.rivasgarcia@gmail.com>
2024-10-15 01:02:38 -0500

    adding resume

--- config/site.ts
+++ config/site.ts
@@ -17,6 +17,6 @@ export const siteConfig = {
     github: "https://github.com/rotios",
     linkedIn: "https://github.com/rotios",
     docs: "https://nextui.org",
-    resume: "https://github.rotios.io/files/RivasResume.pdf"
+    resume: "https://rotios.dev/files/RivasResume.pdf"
   },
 };
